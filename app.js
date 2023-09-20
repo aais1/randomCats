6280172cadea6e2efa6fc7f97ac5c2797468cb66
Ali_Aais <aaisali99@gmail.com>
2023-09-20 07:31:16 -0700

    added download feauture

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-function fetchImg() {
-    let image = document.getElementById("cat-image");
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then(resp => resp.json())
-      .then(json => image.src = json[0].url)
-      .catch(error => console.error("Error fetching cat image:", error));
-  }
-  
-  function btnClick() {
-    let button = document.getElementById("bt");
-    button.addEventListener("click", fetchImg);
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    fetchImg();
+const btDownload = document.getElementById('bt1');
+const catImage = document.getElementById("cat-image");
+
+
+ function fetchImg() {
+  let image = document.getElementById("cat-image");
+  return fetch("https://api.thecatapi.com/v1/images/search")
+    .then(resp => resp.json())
+    .then(json => catImage.src = json[0].url)
+    .catch(error => console.error("Error fetching cat image:", error));
+}
+
+function btnClick() {
+  let button = document.getElementById("bt");
+  button.addEventListener("click",fetchImg);
+}
+
+
+//for downloading 
+btDownload.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.href = catImage.src;
+  console.log(link);
+  link.download = "cat_image.jpg";
+  link.click();
+  link.remove();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+   fetchImg().then(()=>{
     btnClick();
-  });
-  
+  })
+});
